bda6a540ead4cdc8620de0cda20d8a3b89b36ab2
Atuan O <crusifixsso@gmail.com>
2020-08-17 07:40:24 +0100

    Add files via upload

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        
+        <q-toolbar-title class="absolute-center">
+          GoMedia-Auth: ChatBot
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/IndexChat.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex">
+    <h5>Chat page</h5>
+    
+  </q-page>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+</style>
